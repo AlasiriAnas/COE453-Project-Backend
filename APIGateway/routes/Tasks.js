d8146aa3d614f8d645e24ad77e18419f7e061e6d
AlasiriAnas <almanaanas1@gmail.com>
2023-05-13 11:18:02 +0300

    fix: fixing routing => accepting all type of request methods.

--- APIGateway/routes/Tasks.js
+++ APIGateway/routes/Tasks.js
@@ -1,16 +1,21 @@
 import express from 'express';
 const router = express.Router();
 import axios from 'axios';
+import { url } from 'inspector';
 
 
-
+//'http://localhost:5001/Tasks'
 
 //Routing(Forwarding) requests from API gateway to Microservices 
 router.all('/', (req,res)=>{
     
   console.log(req.params.Tasks);
   //url should be replaced with Container Url
-  axios.get('http://localhost:5001/Tasks').then((response)=>{
+  axios({
+    method:req.method,
+    url:'http://localhost:5001/Tasks',
+    headers: req.header,
+    data: req.body}).then((response)=>{
 
     res.send(response.data)
 
